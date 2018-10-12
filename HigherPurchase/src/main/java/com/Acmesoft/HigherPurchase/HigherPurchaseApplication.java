afae6c963a83f7ffdecc6c74b10bad369720efb4
Kolitha Gunaratne <ACM_KolithaG@millenniumitesp.com>
2018-10-12 13:24:19 +0530

    First commit

---
+++ HigherPurchase/src/main/java/com/Acmesoft/HigherPurchase/HigherPurchaseApplication.java
@@ -0,0 +1,12 @@
+package com.Acmesoft.HigherPurchase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HigherPurchaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HigherPurchaseApplication.class, args);
+	}
+}
---
+++ HigherPurchase/src/test/java/com/Acmesoft/HigherPurchase/HigherPurchaseApplicationTests.java
@@ -0,0 +1,16 @@
+package com.Acmesoft.HigherPurchase;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HigherPurchaseApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
